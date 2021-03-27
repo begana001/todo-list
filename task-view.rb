b5786458634f96141d7853cc20cbac6e2fdb6997
Begana Choi <begana001.jpg@gmail.com>
2021-03-27 14:59:46 +0100

    chore(task-view): create TaskView class and skeleton methods

---
+++ task-view.rb
@@ -0,0 +1,18 @@
+class TaskView
+  def display(tasks)
+    tasks.each_with_index do |task, index|
+      done = task.done? ? "[X]" : "[ ]"
+      puts "#{done} #{task[index+1]} - #{task.title}"
+    end
+  end
+
+  def ask_user_for_title
+    puts "What you want to do?"
+    return gets.chomp
+  end
+
+  def ask_user_for_index
+    puts "Index?"
+    return gets.chomp.to_i - 1
+  end
+end
